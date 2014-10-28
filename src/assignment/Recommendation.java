33744d2c72600b45fa4e01aa2235f77a2937163c
Maxime Kjaer <maxime.kjaer@gmail.com>
2014-10-28 15:51:00 +0100

    Removed comments, added tiny tweaks.

--- src/assignment/Recommendation.java
+++ src/assignment/Recommendation.java
@@ -6,17 +6,19 @@ import java.util.Random;
 
 public class Recommendation {
 	
-	//PRIVATE ATTRIBUTS
+	//PRIVATE ATTRIBUTES
 	private static Random random = new Random();
 
-	private static final double EPSILON = 1e-5; // epsilon is threshold for difference in rmse
+	private static final double EPSILON = 2e-4; // epsilon is threshold for difference in rmse
+
 	private static ArrayList<HashMap<Integer, Double>> recommendationStats = new ArrayList<HashMap<Integer, Double>>();
 	
-	//Debug purpose
+	//For debugging purpose
 	private static int row = 10, col = 20, d = 4;
+
 	private static double[][] P = multiplyMatrix(createMatrix(row, d, 1, 5), createMatrix(d, col, 1, 5)), M = new double[row][col];
 	
-	//PUBLIC ATTRIBUTS
+	//PUBLIC ATTRIBUTES
 	/* Etudiant 1 */
 	public static String NAME1 = "Timoté Vaucher";
 	public static int SCIPER1 = 246532;
@@ -46,7 +48,8 @@ public class Recommendation {
 		double[][] startingMatrix = new double[amountRow][amountCol];
 		for (int i = 0; i < amountRow; ++i) {
 			for (int j = 0; j < amountCol; ++j) {
-				startingMatrix[i][j] = startingValue + random.nextGaussian() * variation;
+				int randomSign = random.nextInt(2) == 0 ? -1 : 1;
+				startingMatrix[i][j] = startingValue + random.nextDouble() * variation * randomSign;
 			}
 		}
 		return startingMatrix;
@@ -119,17 +122,9 @@ public class Recommendation {
 					}
 				}
 				if (amountZeroM == P[i].length) index[0] = -1;
-				
 			}
-			if (index[0] == recommendation[i]) {
-				score += 1;
-				//System.out.println("At line : " + i + " granted 1 point");
-			}
-			else if (index[1] == recommendation[i]) {
-				score += 0.5;
-				//System.out.println("At line : " + i + " granted 1/2 point");
-			}
-			//System.out.println("At line : " + i + " max index : " + index[0] + ", second index : " + index[1]);
+			if (index[0] == recommendation[i]) score += 1;
+			else if (index[1] == recommendation[i]) score += 0.5;
 		}
 		return score;
 	}
@@ -285,40 +280,12 @@ public class Recommendation {
 		
 		do {
 			rmseStart = rmseEnd;
-			//System.out.println("RMSE Start: " + rmseStart);
-			/*int order = random.nextInt(Integer.MAX_VALUE) % 4; //The order in which we update the elements is randomly selected
-			switch (order) {
-			case 0 : */
-				for (int i = 0; i < rows; ++i) {
-					for (int j = 0; j < cols; ++j) {
-						U[i][j] = updateUElem(M, U, V, i, j);
-					}
+			for (int i = 0; i < rows; ++i) {
+				for (int j = 0; j < cols; ++j) {
+					U[i][j] = updateUElem(M, U, V, i, j);
 				}
-			/*	break;
-			case 1 :
-				for (int i = rows-1; i >= 0; --i) {
-					for (int j = 0; j < cols; ++j) {
-						U[i][j] = updateUElem(M, U, V, i, j);
-					}
-				}
-				break;
-			case 2 :
-				for (int i = 0; i < rows; ++i) {
-					for (int j = cols-1; j >= 0; --j) {
-						U[i][j] = updateUElem(M, U, V, i, j);
-					}
-				}
-				break;
-			case 3 :
-				for (int i = rows-1; i >= 0; --i) {
-					for (int j = cols-1; j >= 0; --j) {
-						U[i][j] = updateUElem(M, U, V, i, j);
-					}
-				}
-				break;
-			}*/
+			}
 			rmseEnd = rmse(M, multiplyMatrix(U, V));
-			//System.out.println("RMSE End: " + rmseEnd);
 		} while ((rmseStart - rmseEnd) >= EPSILON);
 		return U;
 	}
@@ -329,77 +296,48 @@ public class Recommendation {
 		
 		do {
 			rmseStart = rmseEnd;
-			//System.out.println("RMSE Start: " + rmseStart);
-			/*int order = random.nextInt(Integer.MAX_VALUE) % 4; //The order in which we update the elements is randomly selected
-			switch (order) {
-			case 0 : */
-				for (int i = 0; i < rows; ++i) {
-					for (int j = 0; j < cols; ++j) {
-						V[i][j] = updateVElem(M, U, V, i, j);
-					}
+			for (int i = 0; i < rows; ++i) {
+				for (int j = 0; j < cols; ++j) {
+					V[i][j] = updateVElem(M, U, V, i, j);
 				}
-			/*	break;
-			case 1 :
-				for (int i = rows-1; i >= 0; --i) {
-					for (int j = 0; j < cols; ++j) {
-						V[i][j] = updateVElem(M, U, V, i, j);
-					}
-				}
-				break;
-			case 2 :
-				for (int i = 0; i < rows; ++i) {
-					for (int j = cols-1; j >= 0; --j) {
-						V[i][j] = updateVElem(M, U, V, i, j);
-					}
-				}
-				break;
-			case 3 :
-				for (int i = rows-1; i >= 0; --i) {
-					for (int j = cols-1; j >= 0; --j) {
-						V[i][j] = updateVElem(M, U, V, i, j);
-					}
-				}
-				break;
-			}*/
+			}
 			rmseEnd = rmse(M, multiplyMatrix(U, V));
-			//System.out.println("RMSE End: " + rmseEnd);
 		} while ((rmseStart - rmseEnd) >= EPSILON);
 		return V;
 	}
 
-	//TO BE WORKED ON MOAR !!!
 	public static int[] recommend(double[][] M, int d) {
 		if (!isMatrix(M) || d <= 0) return null;
 		else {
 			final int NUMBER_ITERATION = 10;
 			situateZeros(M);
-			double startingValue = calculateStartingValue(M, d);//
-			//double variation = Math.log(startingValue);
+			double startingValue = calculateStartingValue(M, d);
 			double avgRMSEp = 0, avgRMSEm = 0;
-			//int notNegativeEntry = 0;
-			System.out.println(startingValue);// + " " + variation);
+			System.out.println(startingValue);
+
+			int order = 0;
+
 			for (int i = 0; i < NUMBER_ITERATION; ++i) {
 				System.out.print(i + " ");
 				double[][] U = createStartingMatrix(M.length, d, startingValue, i/NUMBER_ITERATION);
-				//System.out.println(matrixToString(U));
 				double[][] V = createStartingMatrix(d, M[0].length, startingValue, i/NUMBER_ITERATION);
-				//System.out.println(matrixToString(V));
 				double rmseStart = 0, rmseEnd = rmse(M, multiplyMatrix(U, V));
 				do {
 					rmseStart = rmseEnd;
-					//System.out.println("RMSE Start: " + rmseStart);
-					if (random.nextInt(Integer.MAX_VALUE) % 2 == 0) {
+					if (order == 0) {
 						optimizeU(M, U, V);
 						optimizeV(M, U, V);
+						order = (order + 1) % 2;
 					}
 					else {
 						optimizeV(M, U, V);
 						optimizeU(M, U, V);
+						order = (order + 1) % 2;
 					}
+
 					double[][] intermediateUV = multiplyMatrix(U, V);
 					if (intermediateUV.length == 0 || intermediateUV[0].length == 0) break;
 					else rmseEnd = rmse(M, intermediateUV);
-					//System.out.println("RMSE End: " + rmseEnd);
 				} while ((rmseStart - rmseEnd) >= 2*EPSILON);
 				double[][] optimizedUV = multiplyMatrix(U, V);
 				avgRMSEp += rmse(P, optimizedUV);
@@ -410,7 +348,7 @@ public class Recommendation {
 						double[] max = {0, 0};
 						int[] index = {-1, -1};
 						for (int k = 0; k < M[j].length; ++k) {
-							//chose the highest score for indexes of M where there is a zero
+							//choose the highest score for indexes of M where there is a zero
 							if(recommendationStats.get(j).containsKey(k)) {
 								double value = optimizedUV[j][k];
 								if (value > max[1]) {
@@ -467,7 +405,6 @@ public class Recommendation {
 			//DEBUG PURPOSE
 			System.out.println("\navg rmse with original P : " + avgRMSEp/NUMBER_ITERATION);
 			System.out.println("avg rmse with M : " + avgRMSEm/NUMBER_ITERATION);
-			//System.out.println("amount of considered entries : " +notNegativeEntry + "/"+ NUMBER_ITERATION + " : " + (100.*notNegativeEntry)/NUMBER_ITERATION + "%");
 			System.out.println("amount of zeros in M : " + amountZerosM + "/" + col*row + " : " + (100.*amountZerosM)/(col*row) + "%");
 			
 			return output;
@@ -483,146 +420,32 @@ public class Recommendation {
 		for (int i = 0; i < (row * col / 2.); ++i) {
 			M[random.nextInt(row)][random.nextInt(col)] = 0;
 		}
-		long startTime = System.currentTimeMillis();
-		int[] recommendation = recommend(M, d);
-		long stopTime = System.currentTimeMillis();
-		System.out.println("Temps écoulé pour recommender : " + (stopTime-startTime)/1000. + " s");
-		System.out.println("RECOMMMENDATIONS: ");
-		printArray(recommendation);
-		/*int[] real = getRealRecommendation();
-		int score = 0;*/
-		/*ArrayList<Integer> error = new ArrayList<Integer>();
-		for (int i = 0; i < recommendation.length; ++i) {
-			if (recommendation[i] == real[i]) ++score;
-			else {
-				System.out.println("error on line " + i + " expected : " + real[i] + ", recommended : " + recommendation[i]);
-				printArray(P[i]);
-				printArray(M[i]);
-				error.add(i);
-			}
-		}*/
-		System.out.println(/*score + " out of " + recommendation.length + " : " + (100.*score)/recommendation.length + "%\ntrue */"score : " + getScore(recommendation) + "\n");
-		
-		/*for (int i = 0; i < error.size(); ++i) {
-			if (recommendationStats.get(error.get(i)).isEmpty()) System.out.println("line " + error.get(i) + " contains no 0");
-			else {
-				System.out.print("line " + error.get(i) + " : ");
-				for (int j = 0; j < M[i].length; ++j) {
-					if (recommendationStats.get(error.get(i)).containsKey(j)) {
-						double value = recommendationStats.get(error.get(i)).get(j);
-						if (value > 0) System.out.print(j + " [" + value + "], ");
-					}
+
+		double scoreSum = 0;
+
+		for (int i = 0; i < 30; ++i) {
+			long startTime = System.currentTimeMillis();
+			int[] recommendation = recommend(M, d);
+			long stopTime = System.currentTimeMillis();
+			System.out.println("Temps écoulé pour recommender : " + (stopTime-startTime)/1000. + " s");
+			System.out.println("RECOMMMENDATIONS: ");
+			printArray(recommendation);
+			/*int[] real = getRealRecommendation();
+			int score = 0;*/
+			/*ArrayList<Integer> error = new ArrayList<Integer>();
+			for (int i = 0; i < recommendation.length; ++i) {
+				if (recommendation[i] == real[i]) ++score;
+				else {
+					System.out.println("error on line " + i + " expected : " + real[i] + ", recommended : " + recommendation[i]);
+					printArray(P[i]);
+					printArray(M[i]);
+					error.add(i);
 				}
-				System.out.println();
-			}
-		}*/
-		
-		//System.out.println(matrixToString(P));
-		//System.out.println(matrixToString(M));
-		
-		/*double[][] M = {
-			{ 11, 0, 9, 8, 7 },
-			{ 18, 0, 18, 18, 18 },
-			{ 29, 28, 27, 0, 25 },
-			{ 6, 6, 0, 6, 6 },
-			{ 17, 16, 15, 14, 0 }
-		};
-		double U[][] = { {1,1}, {1,1}, {1,1}, {1,1}, {1,1} };
-		double V[][] = { {1,1,1,1,1}, {1,1,1,1,1} };
-		System.out.println(matrixToString(M));
-
-
-		System.out.println("\nTest 2 (isMatrix):");
-		double[][] testMatrix = {
-			{11.0,0.0,9.0,8.0,7.0},
-			{18.0,0.0,18.0,18.0,18.0},
-			{29.0,28.0,27.0,0.0,25.0},
-			{6.0,6.0,0.0,6.0,6.0},
-			{17.0,16.0,15.0,14.0,0.0}
-		};
-		double T[][] = {{ 1, 0, 2},{ 0, 1}};
-		System.out.println("M est une matrice: " + isMatrix(M));
-		System.out.println("testMatrix est une matrice: " + isMatrix(testMatrix));
-		System.out.println("T est une matrice: " + isMatrix(T));
-
-
-		System.out.println("\nTest 3 (multiplyMatrix): ");
-		double[][] A = {{ 1, 0, 2}, { 0, 1, 1}};
-		double[][] B = {{ 1, 2},{ 0, 1},{ 1, 0}};
-		System.out.println("A*B = \n" + matrixToString(multiplyMatrix(A, B)));
-
-
-		System.out.println("\nTest 4 (createMatrix): ");
-		System.out.println("Matrice au pif: \n" + matrixToString(createMatrix(2,3,0,5)));
-
-
-		System.out.println("\nTest 5 (RMSE): ");
-		double[][] m1 = {{ 1, 0, 0},{ 0, 1, 1}};
-		double[][] p1 = {{ 1, 0, 2},{ 0, 1, 1}};
-		System.out.println("Test 1 de RMSE (expect 0.0): " + rmse(m1, p1));
-		double[][] m2 = {{ 1, 0, 1},{ 0, 1, 1}};
-		double[][] p2 = {{ 1, 0, 2},{ 3, 1, 1}};
-		System.out.println("Test 2 de RMSE (expect 0.5): " + rmse(m2, p2));
-		double[][] m3 = {{ 1, 0, 1},{ 0, 1, 1}};
-		double[][] p3 = {{ 1, 0},{ 3, 1}};
-		System.out.println("Test 3 de RMSE (expect -1): " + rmse(m3, p3));
-
-		System.out.println("\nTest 6 (updateUElem): ");
-		double[][] m0 = {
-			{ 11, 0, 9, 8, 7 },
-			{ 18, 0, 18, 18, 18 },
-			{ 29, 28, 27, 0, 25 },
-			{ 6, 6, 0, 6, 6 },
-			{ 17, 16, 15, 14, 0 }
-		};
-		double[][] m1_2 = {
-			{ 0, 8, 9, 8, 7 },
-			{ 18, 0, 18, 18, 18 },
-			{ 29, 28, 27, 0, 25 },
-			{ 6, 6, 0, 6, 6 },
-			{ 17, 16, 15, 14, 0 }
-		};
-
-		double u1[][] = { {2,2}, {2,2}, {2,2}, {2,2}, {2,2} };
-		double v1[][] = { {1,1,1,1,1}, {1,1,1,1,1} };
-		System.out.println("Test 1 de updateUElem (expect ~6.750): " + updateUElem(m0, u1, v1, 0, 0));
-		System.out.println("Test 2 de updateUElem (expect ~6.000): " + updateUElem(m1_2, u1, v1, 0, 0));
-		System.out.println("Test 1 de updateVElem (expect ~7.100): " + updateVElem(m0, u1, v1, 0, 0));
-		System.out.println("Test 2 de updateVElem (expect ~7.750): " + updateVElem(m1_2, u1, v1, 0, 0));/*
-		
-		/*double startingValue = calculateStartingValue(m1_2, 2);
-		System.out.println(startingValue);*/
-		/*double[][] Utest = new double[m1_2.length][2], Vtest = new double[2][m1_2[0].length];
-		for (int i = 0; i < m1_2.length; ++i) {
-			for (int j = 0; j < 2; ++j) {
-				Utest[i][j] = startingValue;
-			}
+			}*/
+			scoreSum += getScore(recommendation);
+			System.out.println(/*score + " out of " + recommendation.length + " : " + (100.*score)/recommendation.length + "%\ntrue */"score : " + getScore(recommendation) + "\n");
 		}
-		for (int i = 0; i < 2; ++i) {
-			for (int j = 0; j < m1_2[0].length; ++j) {
-				Vtest[i][j] = startingValue;
-			}
-		}
-		System.out.println(matrixToString(Utest));
-		System.out.println(matrixToString(Vtest));
-		System.out.println(matrixToString(multiplyMatrix(Utest, Vtest)));*/
-		
-
-		/*
-		System.out.println(matrixToString(createMatrix(20, 20, 0, 5)));
-		createMatrix(5, 5, 0, 5);
-		double[][] A = createMatrix(2, 2, 0, 5);
-		double[][] B = createMatrix(2, 2, 0, 5);
-		System.out.println(matrixToString(A));
-		System.out.println(matrixToString(B));
-		System.out.println(matrixToString(multiplyMatrix(A, B)));
-		double[][] M = createMatrix(2, 2, 0, 5);
-		double[][] P = createMatrix(2, 2, 0, 5);
-		System.out.println(matrixToString(M));
-		System.out.println(matrixToString(P));
-		System.out.println(rmse(M, P));
-		double[][] M2 = {{0,0},{0,0}};
-		System.out.println(rmse(M2, P));
-		*/
+		System.out.println("SCORE TOTAL: " + scoreSum);
+		System.out.println("Moyenne sur 30 itérations: " + scoreSum / 30);
 	}
 }
